60600d4301c04dc89e3c9a6bb363ca76fc21dc3e
xuyangsup <xuyangsup@163.com>
2020-06-23 14:32:42 +0800

    ok

--- demo/vue.config.js
+++ demo/vue.config.js
@@ -1,3 +1,21 @@
 module.exports = {
-    lintOnSave: false // 去掉默认selint  xuyang
+    lintOnSave: false, // 去掉默认selint  xuyang
+    // devServer : {
+    //     open:true,
+    //     port:8089,
+    //     host:"127.0.0.1",
+    //     // 设置代理
+    //     proxy:{
+    //         '/api':{
+    //             target:'',
+    //             changeOrigin:true,
+    //             pathRewrite:{
+    //                 '^/api':''
+    //             }
+    //         }
+    //     }
+    // },
+    publicPath:'./',
+    outputDir:'dist',
+    assetsDir:'static'
 };
